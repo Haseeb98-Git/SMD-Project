da9850b13af82c010afd9e2f0bf61eef9fc8c51d
Haseeb98-Git <hasseebalioriginal@gmail.com>
2025-05-10 15:11:52 +0500

    first commit

---
+++ app/src/main/java/com/haseebali/savelife/Constants.kt
@@ -0,0 +1,10 @@
+package com.haseebali.savelife
+
+object Constants {
+    const val SERVER_URL_EMULATOR = "http://10.0.2.2:80/"
+    const val SERVER_URL_PHONE = "http://192.168.18.58:80/"
+
+    const val SERVER_URL = SERVER_URL_PHONE
+
+    const val SERVER_IMAGES_URL = "${SERVER_URL}image_api/uploads/"
+}
---
+++ app/src/main/java/com/haseebali/savelife/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.haseebali.savelife
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
