7e914509d5e06245ecfa4765353018e9cd0f8010
Haseeb98-Git <hasseebalioriginal@gmail.com>
2025-05-12 14:08:40 +0500

    added keys

--- app/src/main/java/com/haseebali/savelife/Constants.kt
+++ app/src/main/java/com/haseebali/savelife/Constants.kt
@@ -6,5 +6,7 @@ object Constants {
 
     const val SERVER_URL = SERVER_URL_EMULATOR
 
+    const val ONESIGNAL_API_KEY = "os_v2_app_ivjcm52xrrbxdlo7koo4wmahhsw4j5otkgtewomdsang6o33fncfvs4lssh7v6gcndkruyb4fejvudflisdut64anfp7herfjruolfi"
+    const val ONESIGNAL_APP_ID = ""
     const val SERVER_IMAGES_URL = "${SERVER_URL}image_api/uploads/"
 }
